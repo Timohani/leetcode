c587bde50229836623daa89ac2684f5fac0ddc50
Timowa <sd3059272@gmail.com>
2024-12-13 16:28:42 +0500

    change RemoveDuplicatesFromSortedArray

--- src/main/java/org/timowa/Tasks/easy/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/org/timowa/Tasks/easy/RemoveDuplicatesFromSortedArray.java
@@ -1,18 +1,24 @@
 package org.timowa.Tasks.easy;
 
-import java.util.HashSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RemoveDuplicatesFromSortedArray {
-    public static int removeDuplicates(int[] arr) {
-        HashSet<Integer> s = new HashSet<>();
-        int idx = 0;
+    public static int[] removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (!s.contains(arr[i])) {
-                s.add(arr[i]);
-                arr[idx++] = arr[i];
+        List<Integer> uniqueList = new ArrayList<>();
+        uniqueList.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1] && !uniqueList.contains(nums[i])) {
+                uniqueList.add(nums[i]);
             }
         }
-        return idx;
+
+        return uniqueList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
--- src/test/java/org/timowa/Tasks/easy/RemoveDuplicatesFromSortedArrayTest.java
+++ src/test/java/org/timowa/Tasks/easy/RemoveDuplicatesFromSortedArrayTest.java
@@ -9,6 +9,21 @@ import static org.timowa.Tasks.easy.RemoveDuplicatesFromSortedArray.removeDuplic
 class RemoveDuplicatesFromSortedArrayTest {
     @Test
     public void removeDuplicatesTest() {
-        Assertions.assertEquals(5, removeDuplicates(new int[]{0,0,1,1,1,2,2,3,4}));
+        Assertions.assertArrayEquals(new int[]{0,1,2,3,4}, removeDuplicates(new int[]{0,1,1,1,2,2,3,4}));
+    }
+
+    @Test
+    public void removeDuplicatesTestWithAllDuplicates() {
+        Assertions.assertArrayEquals(new int[]{1}, removeDuplicates(new int[]{1,1,1,1,1}));
+    }
+
+    @Test
+    public void removeDuplicatesTestWithNoDuplicates() {
+        Assertions.assertArrayEquals(new int[]{1,2,3,4,5}, removeDuplicates(new int[]{1,2,3,4,5}));
+    }
+
+    @Test
+    public void removeDuplicatesTestWithEmptyArray() {
+        Assertions.assertArrayEquals(new int[]{}, removeDuplicates(new int[]{}));
     }
 }
